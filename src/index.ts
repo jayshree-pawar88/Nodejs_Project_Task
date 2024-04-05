28354d46c6ddd7e9a84a952d53043ddfc0eb2718
jayshree-pawar88 <149353241+jayshree-pawar88@users.noreply.github.com>
2024-04-05 11:58:36 +0530

    added

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import app from './config/express'
 // require('dotenv').config()
 export  const env = process.env
 const port =  process.env.PORT;
-
+console.log("test");
 app.listen(port, () => {
   console.log(`server running on port ${port}.`);
 });
